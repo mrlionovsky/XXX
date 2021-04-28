cd4d3eff729d2b6311d3c266b7ee9980da953d41
mrlionovsky <83355326+mrlionovsky@users.noreply.github.com>
2021-04-28 18:57:17 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+   <meta http-equiv="content-type" content="text/html; charset=windows-1251">
+   <title>Порносайт, для тех у кого медленный интернет.</title>
+   <style>
+   td {font-family: verdana, arial, helvetica; font-size:11px;}
+   h1 {font-family: verdana, arial, helvetica; font-size:16px;} 
+   a { color: #2C2C2C; text-decoration: underline;}
+   .cat td {padding: 6px;}
+
+   </style>
+</head>
+<body text="#000000" bgcolor="#ffffff">
+
+<!-- Здесь очень криво всё. В исходнике одно, на сайте другое. Такой уж стандарт отображения тега PRE :( -->
+<CENTER><pre>
+
+
+================================================================
+    XXXXXXX XXXXXX XXXXXX X   X XXXXXX XXXXXX XXXXXX X    X XXXXXXX
+ X     X X    X X    X X   X X    X X      X    X X   XX    X
+ X     X X    X XXXXXX XXXXX X    X X      XXXXXX X  X X    X
+ X     X X    X X      X   X X    X X      X    X X X  X    X
+ X     X XXXXXX X      X   X XXXXXX XXXXXX X    X XX   X    X
+
+  XXXXX    XXX XXXX   XX     XX XXXX  XXX  XXXXX XX     XX XXXX
+  X   X   X  X X  X   X X   X X X  X  X X  X     X X   X X X  X
+  X   X  X   X XXXX   X  X X  X X  X  X X  XXX   X  X X  X XXXX
+  X   X  X   X  X X   X   X   X X  X  X X  X     X   X   X X  X
+ XXXXXXX X   X X  X   X   X   X XXXX XXXXX XXXXX X   X   X X  X
+===============================================================
+
+</pre>
+
+<h1>...Порносайт, для тех у кого медленный интернет!</h1>
+</CENTER>
+
+
+<!-- Бро, если ты читаешь исходник, то ещё одна баба есть (#36) в исходном коде http://lionovsky.us/bootscreen.html. Именно поэтому их здесь 35, а не 36. --> 
+<table width="800" border="1" align="center">
+<tr>
+<td>
+Все мы иногда оказываемся в местах, где сеть не ловит, или интерент-соединение плохое - например, деревня. Этот ресурс спасёт вашу ситуацию, предоставив вам уникальный контент - порно в текстовом формате!<br><br>
+Его разрабатывали лучшие специалисты из "Порнохуба", "Порнохлаба", а так же специалисты "браззерс". Мы собрали их в Сколково, и сделали для вас сайт, который идеально быстро грузится даже на соединении 1-2 кб/сек.<br>
+Наши пользователи удовлетворены на 100% - как в прямом, так и переносном смысле. <b>Внимание!</b> Работает это только с компа.
+</td>
+</tr> 
+</table>
+
+<table width="800" border="1" align="center">
+<tr>
+<td valign="top" width="20%"><b>Категории:</b><br>- Сперма брызнула на лицо, брр<br>- Жирная попка<br>- Кунилингус <br>- Молодые<br> - 18-летние<br> - 16-летние (убрать, тестер)<br> - ЖМЖ<br>- МЖМ<br>- Групповуха<br> - Анальный кекс<br> - Игрушки<br>- Домашнее<br> - Зрелые<br>- Киска (кис, кис)<br> - Любительское<br> - Моё личное<br>- Раком<br> - Русские <br>- Хардкор<br> - БДСМ<br> - Трансвеститы (не свистите)</td>
+<td valign="top">
+
+    <table width="100%" border="1" class="cat">
+    <tr>
+        <td width="33%"><a href="1.php">Красивая девушка лежит и ждёт тебя!</a></td>
+        <td width="33%"><a href="2.php">Сисястая красотка танцует стриптиз</a></td>
+        <td width="33%"><a href="3.php">Мужик смотрит порно, и дрочит свой багровый член</a></td>
+    </tr>
+    <tr>
+        <td><a href="4.php">Она раздвинула ножки и ждёт тебя.</a></td>
+        <td><a href="5.php">Сисястая красотка показывает свои большие сисьски</a></td>
+        <td><a href="6.php">Очень аппетитные ножки а прож них кое-что ещё аппетитнее</a></td>
+    </tr>
+    <tr>
+        <td><a href="7.php">Она раздвининула свои ножки...</a></td>
+        <td><a href="8.php">На природе среди листьев</a></td>
+        <td><a href="9.php">Жирная попка, вид сзади</a></td>
+    </tr>
+    <tr>
+        <td><a href="10.php">Она дрочит её левой рукой</a></td>
+        <td><a href="11.php">Зрелая красотка показывает свои большие сисьски</a></td>
+        <td><a href="12.php">Её сфоткал парень прямо во время процесса!</a></td>
+    </tr>
+    <tr>
+        <td><a href="13.php">Горячее это среди холодного дня</a></td>
+        <td><a href="14.php">Она разделась и сосёт как пылесос!</a></td>
+        <td><a href="15.php">Сидит на стуле и течёт</a></td>
+    </tr>
+    <tr>
+        <td><a href="16.php">Две девушки и один мужчина!</a></td>
+        <td><a href="17.php">Впервые разделась на пляже и показывает всем свою красоту</a></td>
+        <td><a href="18.php">Я. Вид из зеркала в моей комнате.</a></td>
+    </tr>
+    <tr>
+        <td><a href="19.php">Модный показ в кружевном белье.</a></td>
+        <td><a href="20.php">Села на большой и багровый и получает настоящее удовольствие.</a></td>
+        <td><a href="21.php">Самая лучшая одежда для дома!</a></td>
+    </tr>
+    <tr>
+        <td><a href="22.php">Большой и багровый ждёт свою красотку.</a></td>
+        <td><a href="23.php">Она готова к приключениям. А ты?</a></td>
+        <td><a href="24.php">Вечером с новой игрушкой</a></td>
+    </tr>
+    <tr>
+        <td><a href="25.php">Две красотки и один мужчина прямо на камеру!</a></td>
+        <td><a href="26.php">Теребит себя за святое и получает массу удовольствия.</a></td>
+        <td><a href="27.php">Семья ММЖМ вышла погулять</a></td>
+    </tr>
+    <tr>
+        <td><a href="28.php">Пьяная, помятая, пионервожатая!</a></td>
+        <td><a href="29.php">Милый, у меня новый лифчик!</a></td>
+        <td><a href="30.php">Японка косплейт кого-то из аниме. Пощупай это!</a></td>
+    </tr>
+    <tr>
+        <td><a href="31.php">Мужчина вылизал её так что она долго охала</a></td>
+        <td><a href="32.php">Душ, пар и прекрасный вид из-за шторки в ванной!</a></td>
+        <td><a href="33.php">Сидит усталая после работы и ждёт горячего приёма</a></td>
+    </tr>
+    <tr>
+        <td><a href="34.php">Чёрная игрушка вошла не туда, но она получила массу удовольствия!</a></td>
+        <td><a href="35.php">Она получает удвоольствие правой рукой!</a></td>
+        <td> </td>
+    </tr>
+    </table>
+
+
+</td>
+</tr> 
+<tr class="ads"><td colspan="2"></td></tr>
+</table>
+
+<br>
+<center>
+<small>
+<b>Строго для лиц старше 40 лет!</b><br>
+<a href="http://lionovsky.us">(c) Lionovsky.us, 2020</a>
+</small>
+</center>     
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(47424376, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/47424376" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+</body>
+</html>
